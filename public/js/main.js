ad4b37f693d09fba609f09cb09d286c70225fce6
FApew <112774976+FApew@users.noreply.github.com>
2024-07-01 20:26:22 +0200

    AddedMovingPlayers

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,12 @@
 //### Coded by FA_pew ###
+
 //Server
-const socket = new io("https://fa-racecars.onrender.com/")
-//const socket = new io("http://localhost:3500/")
+//const socket = new io("https://fa-racecars.onrender.com/")
+const socket = new io("http://localhost:3500/")
 
 //Dependencies
 import * as THREE from "three"
-import * as CANNON from "https://cdn.jsdelivr.net/npm/cannon-es@0.20.0/+esm"
 import WebGL from "three/addons/capabilities/WebGL.js"
-import CannonDebugger from "https://cdn.jsdelivr.net/npm/cannon-es-debugger@1.0.0/+esm"
 import Stats from "three/addons/libs/stats.module.js"
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js"
 
@@ -20,17 +19,13 @@ const idElm = document.getElementById("idTxt")
 
 //Scenes & loaders
 const scene = new THREE.Scene()
-const world = new CANNON.World({
-    gravity: new CANNON.Vec3(0, -9.806, 0),
-})
 const loader = new GLTFLoader()
+const clock = new THREE.Clock()
 
 //Vars
-let pi = Math.PI, users = 0, userID = 0
+let pi = Math.PI, userID = 0, worldBodies, bodies = new Map(), btn = {w: 0, a: 0, s: 0, d: 0, shift: 0}
 
 if (WebGL.isWebGLAvailable()) {
-    socket.emit("join")
-
     //Camera
     const camera = new THREE.PerspectiveCamera(75, container.clientWidth / container.clientHeight, 0.1, 1000)
     camera.position.z = 5
@@ -43,77 +38,11 @@ if (WebGL.isWebGLAvailable()) {
     renderer.setAnimationLoop(animate)
     container.appendChild(renderer.domElement)
 
-    //Physic plane
-    const cPlane = new CANNON.Body({
-        type: CANNON.Body.STATIC,
-        shape: new CANNON.Plane(),
-    })
-    cPlane.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
-    world.addBody(cPlane)
-
-    //Obj Init
-    let gObjs = [], pObjs = [], objGroup = new THREE.Group()
-    for (let i = 0; i < Objs.length; i++) {
-        let obj = Objs[i]
-        //Load Model
-        loader.load(`../assets/${obj.model}`, (gltf) => {
-            const model = gltf.scene
-            model.traverse((child) => {
-                if (child.isMesh) {
-                    const material = new THREE.MeshStandardMaterial({
-                        color: child.material.color,
-                        map: child.material.map,
-                        side: THREE.DoubleSide
-                    })
-                    child.material = material
-                    child.castShadow = true
-                    child.receiveShadow = true
-                    child.geometry.computeVertexNormals()
-                }
-            })
-
-            //Apply Scale
-            model.scale.set(obj.s, obj.s, obj.s)
-
-            //Get Size
-            let box = new THREE.Box3().setFromObject(model)
-            let size = new THREE.Vector3()
-            box.getSize(size)
-
-            //Add Graphics
-            model.position.set(obj.pos.x, obj.pos.y + size.y/2, obj.pos.z)
-            model.rotation.set(0, obj.r, 0)
-            gObjs[i] = model
-            objGroup.add(model)
-
-            //Add Physics
-            if (obj.p) {
-                const pObj = new CANNON.Body({
-                    shape: new CANNON.Box(new CANNON.Vec3(size.x/2, size.y/2, size.z/2)),
-                    position: new CANNON.Vec3(obj.pos.x, obj.pos.y + size.y/2, obj.pos.z),
-                    mass: obj.g == 1 ? 1 : 0
-                })
-                pObj.quaternion.setFromEuler(0, obj.r, 0)
-                world.addBody(pObj)
-                pObj.sleep()
-                pObjs[i] = pObj
-            } else {
-                pObjs[i] = 0
-            }
-        })
-    }
-
-    //Update scene
-    scene.add(objGroup)
-
     //Stats
     let stats = new Stats()
     stats.domElement.style.position = "absolute"
     stats.domElement.style.top = "50px"
     container.appendChild( stats.dom )
-    
-    //Cannon Debugger
-    const cannonDebugger = new CannonDebugger(scene, world, {})
 
     //On resize event
     window.onresize = () => {
@@ -122,50 +51,158 @@ if (WebGL.isWebGLAvailable()) {
         renderer.setSize(container.clientWidth, container.clientHeight )
     }
     
-    //Multiplayer config
-    let playerArr = []
+    //Get userID
     socket.on("join", (data) => {
-        users = data
         if (userID == 0) {
-            userID = users[users.length-1]
+            userID = data[data.length-1]
             idElm.innerHTML = `ID: ${userID}`
         }
     })
-    socket.on("leave", (data) => {
-        users = data
-    })
-    
-    
-    //Animate
-    let t = Date.now(), dt
-    function animate() {
 
-        //Physic update
-        dt = Date.now() - t
-        world.step(1/60, dt)
-        t = Date.now()
+    //Get inputs
+    document.addEventListener("keydown", (e) => {
+        switch (e.key) {
 
-        //Object Update
-        for (let i = 0; i < gObjs.length; i++) {
-            try {
-                if (pObjs[i] != 0) {
-                    let gObj = gObjs[i], pObj = pObjs[i]
-                    if (pObj.velocity.length() < 0.01) {
-                        pObj.sleep()
-                    } else {
-                        gObj.position.copy(pObj.position)
-                        gObj.quaternion.copy(pObj.quaternion) 
-                    }
-                }
-            } catch (e) {
-                console.warn(e)
+            case "w":
+            case "W":
+            case "ArrowUp": {
+                btn.w = 1
+                break 
+            }
+
+            case "a":
+            case "A":
+            case "ArrowLeft": {
+                btn.a = 1
+                break
+            }
+
+            case "s":
+            case "S":
+            case "ArrowDown": {
+                btn.s = 1
+                break
+            }
+
+            case "d":
+            case "D":
+            case "ArrowRight": {
+                btn.d = 1
+                break
+            }
+
+            case "Shift": {
+                btn.shift = 1
+                break
             }
         }
+        socket.emit("input", userID, btn)
+    })
+
+    document.addEventListener("keyup", (e) => {
+        switch (e.key) {
+
+            case "w":
+            case "W":
+            case "ArrowUp": {
+                btn.w = 0
+                break 
+            }
+
+            case "a":
+            case "A":
+            case "ArrowLeft": {
+                btn.a = 0
+                break
+            }
+
+            case "s":
+            case "S":
+            case "ArrowDown": {
+                btn.s = 0
+                break
+            }
+
+            case "d":
+            case "D":
+            case "ArrowRight": {
+                btn.d = 0
+                break
+            }
+
+            case "Shift": {
+                btn.shift = 0
+                break
+            }
+        }
+        socket.emit("input", userID, btn)
+    })
+    
+    //Update physics
+    socket.on("update", (data) => {
+        //Get the data
+        worldBodies = data.bodies;
+        let IDs = data.bodies.map(body => (body.ID))
+        worldBodies.forEach((bData) => {
+            let body = bodies.get(bData.ID)
+    
+            // Create a body
+            if (!body) {
+                body = newBody(bData)
+                bodies.set(bData.ID, body)
+            }
+        })
+    
+        //Remove Unused
+        bodies.forEach((body, id) => {
+            if (!IDs.includes(id)) {
+                bodies.delete(id)
+                scene.remove(body)
+                if (body.geometry) {
+                    body.geometry.dispose()
+                }
+                if (body.material) {
+                    if (Array.isArray(body.material)) {
+                        body.material.forEach(material => material.dispose())
+                    } else {
+                        body.material.dispose()
+                    }
+                }
+            }
+        })
+    
+        //Position Update
+        worldBodies.forEach((bData) => {
+            let body = bodies.get(bData.ID)
+            if (body) {
+                //Target Positions
+                body.tPos = new THREE.Vector3(...bData.position)
+                body.tQuat = new THREE.Quaternion(...bData.quaternion)
+            }
+        })
+    })
+    
+    //Animate
+    function animate() {
+        let dt = clock.getDelta()
+
+        bodies.forEach((body) => {
+            body.position.lerp(body.tPos, dt * 10)
+            body.quaternion.slerp(body.tQuat, dt * 10)
+        })
 
         //Updates
-        console.log(users.length)
         stats.update()
-        cannonDebugger.update()
         renderer.render(scene, camera)
+    }
+
+    //Create new Body
+    function newBody() {
+        const body = new THREE.Mesh(
+            new THREE.BoxGeometry(.5, .1, .3),
+            new THREE.MeshBasicMaterial({color: 0xffffff})
+        )
+        scene.add(body)
+        return body
     }
 }
--- server.js
+++ server.js
@@ -1,13 +1,17 @@
+//### Coded by FA_pew ###
+
+//Server Dependecies
 import express from "express"
 import { Server } from "socket.io"
 import path from "path"
 import { fileURLToPath } from "url"
 
+//Dependencies
+import * as CANNON from "cannon-es"
+
+//### Server ###
 const dir = path.dirname(fileURLToPath(import.meta.url))
 const PORT = process.env.PORT || 3500
-
-
-let users = []
 
 const app = express()
 app.use(express.static(path.join(dir, "public")))
@@ -28,32 +32,127 @@ const io = new Server(expServer, {
     }
 })
 
+//Server World
+
+//New Users
 io.on("connection", socket => {
     
-    socket.on("join", () => {
-        let IP = socket.handshake.headers['x-real-ip'] || socket.handshake.headers['x-forwarded-for'] || socket.handshake.address
-        users.push(socket.id)
-        console.log(`Connection: ${users.length} users [${IP}]`)
-        console.log(users)
-        io.emit("join", users)
-    })
+    //Join event
+    let IP = socket.handshake.headers['x-real-ip'] || socket.handshake.headers['x-forwarded-for'] || socket.handshake.address
+    users.push(socket.id)
+    playerhandler(1, socket.id)
+    console.log(`Connection: ${users.length} users [${IP}]`)
+    console.log(users)
+    io.emit("join", users)
 
+
+    //Disconnection event
     socket.on("disconnect", () => {
-        try {
-            users.splice(users.indexOf(socket.id), 1)
-        } catch (e) {
-            console.error(e)
-        }
+        playerhandler(0, socket.id)
+        users.splice(users.indexOf(socket.id), 1)
         console.log(`Disconnection: ${users.length} users`)
         console.log(users)
         io.emit("leave", users)
     })
+
+    //Input change
+    socket.on("input", (id, btn) => {
+        let idx = users.indexOf(id)
+        if (btn.w) {
+            players[idx].W = 1
+        } else {
+            players[idx].W = 0
+        }
+        if (btn.a) {
+            players[idx].A = 1
+        } else {
+            players[idx].A = 0
+        }
+        if (btn.s) {
+            players[idx].S = 1
+        } else {
+            players[idx].S = 0
+        }
+        if (btn.d) {
+            players[idx].D = 1
+        } else {
+            players[idx].D = 0
+        }
+        if (btn.shift) {
+            players[idx].SHIFT = 1
+        } else {
+            players[idx].SHIFT = 0
+        }
+    })
 })
 
-function parseHeader(header) {
-    for (const directive of header.split(",")[0].split(";")) {
-      if (directive.startsWith("for=")) {
-        return directive.substring(4);
-      }
+//Update clock
+setInterval(update, 50/3)
+
+//Define world & vars
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.806, 0),
+})
+let pi = Math.PI, users = [], players = []
+
+//Physic plane
+const cPlane = new CANNON.Body({
+    type: CANNON.Body.STATIC,
+    shape: new CANNON.Plane(),
+})
+cPlane.quaternion.setFromEuler(-Math.PI / 2, 0, 0)
+world.addBody(cPlane)
+cPlane.id = -1
+
+//Create & Remove Physical players
+function playerhandler(b, id) {
+    if (b) {
+        const player = new CANNON.Body({
+            shape: new CANNON.Box(new CANNON.Vec3(0.25, 0.05, 0.15)),
+            position: new CANNON.Vec3(0, 1, 0),
+            mass: 1,
+        })
+        player.ID = id
+        player.W = 0
+        player.A = 0
+        player.S = 0
+        player.D = 0
+        players[users.indexOf(id)] = player
+        world.addBody(player)
+    } else {
+        let idx = users.indexOf(id)
+        world.removeBody(players[idx])
+        players.splice(idx, 1)
     }
-  }
+}
+
+//Update function
+function update() {
+
+    players.forEach((player) => {
+        if (player.W) {
+            player.position.z-=.3
+        }
+        if (player.A) {
+            player.position.x-=.3
+        }
+        if (player.S) {
+            player.position.z+=.3
+        }
+        if (player.D) {
+            player.position.x+=.3
+        }
+    })
+
+    world.step(1/60)
+
+    const worldState = {
+        bodies: world.bodies.filter(e => e.id != -1).map(body => ({
+            ID: body.ID,
+            position: body.position.toArray(),
+            quaternion: body.quaternion.toArray()
+        }))
+    }
+    //Emit update
+    io.emit("update", worldState)
+}
